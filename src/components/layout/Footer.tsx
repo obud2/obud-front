e79578eceed73f5f5dc3e7a6da69086b488a74d8
sckimynwa <tsb03672@naver.com>
2023-11-05 08:11:10 +0900

    feat: remove about

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -50,9 +50,12 @@ const Footer = ({ footerHide }: Props) => {
           <div className="footer-info-container">
             <div className="footer-contents-container">
               <div className="footer-contact-menu-items">
-                <div className="footer-button" onClick={() => onClickRouter('/about')}>
-                  About
-                </div>
+                {/* TODO: fix later */}
+                {/* {!matchese && (
+                  <div className="footer-button" onClick={() => onClickRouter('/about')}>
+                    About
+                  </div>
+                )} */}
 
                 <div className="footer-button" onClick={() => onClickRouter('/contact')}>
                   Contact Us
