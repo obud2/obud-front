340123f17976cc3e8957b0d0a2796ae4bf60ad06
Fresh Lim <fresh.lim@mathpresso.com>
2023-11-19 21:46:36 +0900

    fix: 안쓰는 info 메서드 제거

--- src/service/InfoService.jsx
+++ src/service/InfoService.jsx
@@ -1,15 +1,5 @@
 import axiosInstance from '@/constants/AxiosInstance';
 
-const getListAll = () => {
-  return new Promise((resolve) => {
-    axiosInstance.get('bbs/info/').then((response) => {
-      if (response.data && response.data.value) {
-        resolve(response.data.value);
-      }
-    });
-  });
-};
-
 const info = (id) => {
   return new Promise((resolve) => {
     axiosInstance.get(`bbs/info/${id}`).then((response) => {
@@ -22,7 +12,6 @@ const info = (id) => {
 
 const InfoService = {
   info,
-  getListAll,
 };
 
 export default InfoService;
