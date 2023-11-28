a0558fdfd1851dd444b2cf8eba10cb5e6c170b91
Fresh Lim <fresh.lim@mathpresso.com>
2023-11-28 10:59:53 +0900

    fix: 대소문자 리다이렉트는 api 호출과 리소스 호출의 경우를 제외한다

--- middleware.ts
+++ middleware.ts
@@ -2,11 +2,24 @@ import type { NextRequest } from 'next/server';
 import { NextResponse } from 'next/server';
 
 export const middleware = (req: NextRequest) => {
-  if (req.nextUrl.pathname === req.nextUrl.pathname.toLowerCase()) {
+  if (req.nextUrl.pathname === req.nextUrl.pathname.toLowerCase() || req.nextUrl.origin.includes('api') || req.nextUrl.pathname.includes('.') || req.nextUrl.pathname.includes('_') || req.nextUrl.pathname.includes('OD')) {
     return NextResponse.next();
   }
 
   return NextResponse.redirect(
     new URL(req.nextUrl.origin + req.nextUrl.pathname.toLowerCase()),
   );
+};
+
+export const config = {
+  matcher: [
+    /*
+     * Match all request paths except for the ones starting with:
+     * - api (API routes)
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico (favicon file)
+     */
+    '/((?!api|_next/static|_next/image|favicon.ico).*)',
+  ],
 };
