bec545b2ed0b0b2ed63e2b048e2a1118b22ac945
sckimynwa <tsb03672@naver.com>
2023-12-20 21:56:33 +0900

    feat: apply banner link

--- src/components/class/BannerItem.tsx
+++ src/components/class/BannerItem.tsx
@@ -1,35 +1,69 @@
-import { useRouter } from 'next/router';
-
 import CustomImage from '@components/common/image/CustomImage';
-import { SStudioItem } from './SectionStudioItem.styled';
 import { Banner } from '@/entities/banner';
+import Link from 'next/link';
+import styled from 'styled-components';
+import { MOBILE } from '@/styled/variablesStyles';
 
 type Props = {
   banner: Banner;
 };
 
 const BannerItem = ({ banner }: Props) => {
-  const router = useRouter();
-
-  const { id, name, imageUrl } = banner;
-
-  const handleDetail = () => {
-    router.push(`/class/${id}`);
-  };
+  const { imageUrl, linkUrl } = banner;
 
   return (
-    <SStudioItem onClick={handleDetail} isActive={!!imageUrl}>
-      <div className="studio-item-images-container">
-        <div className="studio-item-image">
-          <CustomImage className="studio-image-1" src={imageUrl} width={800} height={800} alt="studio-images" />
+    <SBannerItem>
+      <Link href={linkUrl}>
+        <div className="banner-item-images-container">
+          <div className="banner-item-image">
+            <CustomImage className="banner-image-1" src={imageUrl} width={800} height={800} alt="banner-images" />
+          </div>
         </div>
-      </div>
-
-      <div className="studio-item-contents-container">
-        <p className="studio-item-title">{name || ''}</p>
-      </div>
-    </SStudioItem>
+      </Link>
+    </SBannerItem>
   );
 };
 
 export default BannerItem;
+
+const SBannerItem = styled.div`
+  width: 100%;
+  height: 100%;
+
+  position: relative;
+
+  display: flex;
+  flex-direction: column;
+  margin-bottom: 15px;
+
+  ${MOBILE} {
+    cursor: pointer;
+
+    margin-bottom: 12px;
+  }
+
+  .banner-item-images-container {
+    width: 100%;
+    min-width: 130px;
+    height: auto;
+
+    aspect-ratio: 1 / 1;
+
+    position: relative;
+    object-fit: cover;
+
+    overflow: hidden;
+    cursor: pointer;
+
+    .banner-item-image {
+      top: 0;
+      left: 0;
+      position: absolute;
+    }
+
+    .banner-image-1 {
+      z-index: 1;
+      transition: opacity 0.3s ease-in-out;
+    }
+  }
+`;
--- src/entities/banner.ts
+++ src/entities/banner.ts
@@ -2,6 +2,7 @@ export type Banner = {
   id: number;
   name: string;
   imageUrl: string;
+  linkUrl: string;
   order: number;
   createdAt: string;
   isShow: boolean;
