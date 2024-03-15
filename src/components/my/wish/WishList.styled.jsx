afabe0d4c30f6d9867cd442586292d6be2841420
anna8481 <nahn8481@gmail.com>
2024-03-15 11:17:07 +0900

    style: update mobile wish list page

--- src/components/my/wish/WishList.styled.jsx
+++ src/components/my/wish/WishList.styled.jsx
@@ -13,7 +13,7 @@ export const SWishList = styled.div`
 
     display: flex;
     align-items: center;
-    justify-content: space-between;
+    justify-content: flex-end;
 
     margin-bottom: 10px;
 
